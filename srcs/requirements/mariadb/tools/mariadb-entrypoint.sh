80d1911c5e08c0804d537705057337a152583b37
Helena Utzig <helenautzig@gmail.com>
2025-06-05 17:57:03 +0300

    WIP

--- srcs/requirements/mariadb/tools/mariadb-entrypoint.sh
+++ srcs/requirements/mariadb/tools/mariadb-entrypoint.sh
@@ -31,7 +31,6 @@ mariadb --socket=/run/mysqld/mysqld.sock -u root <<-EOSQL
     CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;
     CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PWD}';
     GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO '${DB_USER}'@'%';
-    GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO '${DB_USER}'@'localhost';
     ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PWD}';
     FLUSH PRIVILEGES;
 EOSQL
