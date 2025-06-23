d30b78c8281bb00c70e77ba2b8429af1e8106006
Helena Utzig <helenautzig@gmail.com>
2025-06-23 10:55:47 +0300

    DB: remove mariadb health check

--- srcs/requirements/mariadb/tools/mariadb-entrypoint.sh
+++ srcs/requirements/mariadb/tools/mariadb-entrypoint.sh
@@ -14,23 +14,6 @@ if [ ! -d "/var/lib/mysql/mysql" ]; then
     mariadb-install-db --user=mysql --datadir=/var/lib/mysql
 fi
 
-#echo "[+] Starting MariaDB temporarily for setup..."
-#mysqld --user=mysql --skip-networking --socket=/run/mysqld/mysqld.sock &
-#pid="$!"
-
-# Wait for socket to be available (max 30s)
-#timeout=30
-#while ! mariadb --socket=/run/mysqld/mysqld.sock -u root &>/dev/null; do
-#    echo "Waiting for MariaDB to start..."
-#    sleep 1
-#    timeout=$((timeout - 1))
-#    if [ "$timeout" -le 0 ]; then
-#        echo "MariaDB failed to start within timeout."
-#        kill "$pid" 
-#        exit 1
-#    fi
-#done
-
 echo "[+] Running database setup..."
 
 # Start MariaDB in bootstrap mode to run initialization SQL commands without starting the full server
@@ -62,17 +45,6 @@ mysqld --user=mysql --bootstrap <<EOF
     -- Reload privilege tables again to apply all changes
     FLUSH PRIVILEGES;
 EOF
-
-#mariadb --socket=/run/mysqld/mysqld.sock -u root <<-EOSQL
-#    CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;
-#    CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PWD}';
-#    GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO '${DB_USER}'@'%';
-#    ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PWD}';
-#    FLUSH PRIVILEGES;
-#EOSQL
-
-#kill "$pid"
-#wait "$pid"
 
 echo "[+] Starting MariaDB normally..."
 exec mysqld --user=mysql
